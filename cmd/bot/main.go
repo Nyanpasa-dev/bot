6b55a18b17f2c5040873b2d471a55959449224da
nyanpasa <36734086+Nyanpasa-dev@users.noreply.github.com>
2024-08-15 01:59:31 +0500

    add help command and little refactoring

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -33,14 +33,26 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message == nil {
-			continue
+
+		switch update.Message.Command() {
+		case "help":
+			helpCommand(bot, update.Message)
+		default:
+			defaultBehaviour(bot, update.Message)
 		}
 
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
-
-		bot.Send(msg)
 	}
+}
+
+func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Available commands:\n/help - show this message\n")
+
+	bot.Send(msg)
+}
+
+func defaultBehaviour(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote: "+inputMessage.Text)
+
+	bot.Send(msg)
 }
